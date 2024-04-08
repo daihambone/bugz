276b85c201d3e478aec661784c9bf36be741b2cf
daihambone <waynesamuel@gmail.com>
2024-04-08 21:41:02 +0100

    test 1

---
+++ js/settings.js
@@ -0,0 +1,21 @@
+const settings = {
+    canvas: {
+        width: 800,
+        height: 800,
+    },
+    bugs: {
+        quantity: 100,
+    },
+	world: {
+		base_speed: 0.001,
+		base_size: 64,
+		border_to_unit: 1,
+		border_size: null,
+	},
+	control: {
+		running: true,
+	},
+};
+
+
+console.log('Settings:', settings);
---
+++ server.bat
@@ -0,0 +1,10 @@
+
+
+
+
+
+
+node node_modules/http-server/bin/http-server -c-1
+
+
+
